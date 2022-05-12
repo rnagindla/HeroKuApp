79830a5a1c96e3e143ec1d492a9ed63a99ca9f76
rnagindla <ramesh.be231@gmail.com>
2022-05-11 20:26:56 -0400

    RemoteWebDriver updated.

--- test/HeroKuAppTest.cs
+++ test/HeroKuAppTest.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HeroKuApp.page;
+using OpenQA.Selenium.Remote;
 
 namespace HeroKuApp.test;
 
@@ -14,9 +15,30 @@ public class HeroKuAppTest
     private IWebDriver driver;
     private WelcomePage welcomePage;
     private AddRemoveElementsPage addRemoveElementsPage;
+    private Boolean runLocal = false;
     [SetUp]
     public void setUp() {
-        driver = new ChromeDriver();
+        if (runLocal)
+        {
+            driver = new ChromeDriver();
+        }
+        else
+        {
+           
+            var sauceOptions = new Dictionary<string, object>
+            {
+                  ["name"] = TestContext.CurrentContext.Test.Name
+            };
+
+                var chromeOptions = new ChromeOptions
+                {
+                    BrowserVersion = "latest",
+                    PlatformName = "Windows 10"
+                };
+                chromeOptions.AddAdditionalOption("sauce:options", sauceOptions);
+                driver = new RemoteWebDriver(new Uri("https://oauth-ramesh.be231-1b284:df659765-3082-4b4a-9c30-1f100ae5fad7@ondemand.us-west-1.saucelabs.com:443/wd/hub"), chromeOptions.ToCapabilities(), TimeSpan.FromSeconds(30));
+            
+        }
         driver.Manage().Window.Maximize();
         driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
     }
